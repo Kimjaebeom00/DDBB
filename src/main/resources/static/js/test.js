39b67a8ef7afad3182c5f2f909c7903b25c70cb5
srkim <srkim@ethree.co.kr>
2023-03-31 17:10:12 +0900

    view 디렉토리에 분리

---
+++ src/main/resources/static/js/test.js
@@ -0,0 +1,27 @@
+function check(form)
+{
+    if(form.userid.value=="kimjaebeom" && form.userpassword.value=="1234")
+    {
+        window.open('target.html')
+    }
+    else{
+        alert("아이디 및 비밀번호가 다릅니다.")
+    }
+}
+
+function update(form)
+{
+   
+    
+        window.open('update.html')
+    
+   
+}
+function save(form)
+{
+   
+    
+        window.open('save.html')
+    
+   
+}
