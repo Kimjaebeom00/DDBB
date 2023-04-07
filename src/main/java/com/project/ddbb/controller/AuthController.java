b87fb6804ac155ed49155b7d69e2785e8f84c997
jaebeomKim <process68@naver.com>
2023-04-07 20:57:02 +0900

    postmaping sign_in부분 아이디및 비밀번호 유효성검사 기능 추가했습니다.

--- src/main/java/com/project/ddbb/controller/AuthController.java
+++ src/main/java/com/project/ddbb/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.project.ddbb.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,11 +24,31 @@ public class AuthController {
      * 로그인 처리
      * @return
      */
-    @PostMapping("/signIn")
-    public String signInProcess() {
+//    @PostMapping("/signIn")
+//    public String signInProcess() {
+//
+//        return "redirect:/project/home";
+//    }
+    /**
+     * 로그인 유효성 처리
+     * @return
+     */
 
-        return "redirect:/project/home";
+    @PostMapping("/signIn")
+    public String signInProcess(HttpServletRequest request) {
+        String userid = request.getParameter("userid");
+        String password = request.getParameter("password");
+
+        // userid와 password 검증 로직
+        if (userid != null && password != null && !userid.isEmpty() && !password.isEmpty()) {
+            // 로그인 성공
+            return "redirect:/project/home";
+        } else {
+            // 로그인 실패
+            return "redirect:/signIn";
+        }
     }
+
 
     /**
      * 회원가입 화면
