bc898b1d7a209bdb432f6952c8768b3091905a1b
HyeokThing <djawogur2018@naver.com>
2023-04-29 17:44:46 +0900

    코드비교용 예제코드 추가하였습니다. * 추후 삭제 될 것

--- src/test/java/com/project/ddbb/CodeCompareTest.java
+++ src/test/java/com/project/ddbb/CodeCompareTest.java
@@ -1,5 +1,6 @@
 package com.project.ddbb;
 
+import com.project.ddbb.domain.vo.TestCode;
 import com.project.ddbb.domain.vo.CodeVO;
 import com.project.ddbb.domain.service.CodeCompareService;
 import org.junit.jupiter.api.Test;
@@ -18,20 +19,20 @@ public class CodeCompareTest {
     @Test
     void save() {
         CodeVO cvo = codeService.FindById(testCurrentProjectId);
-
+        TestCode tc = new TestCode();
         if (cvo == null) // 프로젝트에 코드가 없을 경우
         {
             cvo = new CodeVO();
             cvo.setCodeId(1L);
             cvo.setProjectId(testCurrentProjectId);
             cvo.setTitle("DDBB");
-            cvo.setBeforeCode("Before Contents");
+            cvo.setBeforeCode(tc.getTestCode1());
             cvo.setCurrentCode(null);
             codeService.saveCode(cvo);
         }
         else if (cvo.getCurrentCode() == null) // 프로젝트에 이전 코드만 있고, 현재 코드는 없을 경우
         {
-            cvo.setCurrentCode("Current Contents");
+            cvo.setCurrentCode(tc.getTestCode2());
             codeService.updateCode(cvo);
         }
         else // 프로젝트에 이전, 현재 코드가 모두 있는 경우
