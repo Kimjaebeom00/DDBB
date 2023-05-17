ba9585b9091998673071efaf906aac3653bc0454
SungRyeolKim <kimsr0426@inha.edu>
2023-05-17 21:51:05 +0900

    goToInsertCode 자바스크립트 함수 삭제(미사용)

--- src/main/resources/static/js/service.js
+++ src/main/resources/static/js/service.js
@@ -8,11 +8,3 @@ let projectInfoInit = function() {
     // var projectMembers = api.projectMembers(projectId);//api.js호출한것이다
 }
 
-const goToInsertCode = function() {
-    const projectId = $('#currentProjectId').val();
-    const form = $('#InsertCodeForm');
-
-    $('#projectIdForCode').val(projectId);
-
-    form.submit();
-}
