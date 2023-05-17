e8bdc9b65750cf41c8e1f6c281afa019de1cbd55
SungRyeolKim <kimsr0426@inha.edu>
2023-05-17 23:06:41 +0900

    코드 비교 자바스크립트 커밋 goToInsertCode 함수 삭제 취소(잘못 보고 지워버렸습니다.. 죄송)

--- src/main/resources/static/js/service.js
+++ src/main/resources/static/js/service.js
@@ -4,7 +4,36 @@ let projectInfoInit = function() {
 
     target.addClass('selected');
 
+    let comparedCodeInfoMap = api.compareCode(projectId);
+    console.log(comparedCodeInfoMap);
+    const beforeCodeContainer = $('.before_code');
+    const currentCodeContainer = $('.current_code');
 
-    // var projectMembers = api.projectMembers(projectId);//api.js호출한것이다
+    comparedCodeInfoMap.beforeList.forEach((beforeLine, index) => {
+        if(comparedCodeInfoMap.modifiedList.indexOf(index) >= 0) {
+            beforeCodeContainer.append('<p class="modified_code">' + beforeLine + '</p>');
+        } else if(comparedCodeInfoMap.addedList.indexOf(index) >= 0) {
+            beforeCodeContainer.append('<p class="added_code">' + beforeLine + '</p>');
+        } else {
+            beforeCodeContainer.append('<p>' + beforeLine + '</p>');
+        }
+    });
+    comparedCodeInfoMap.currentList.forEach((currentLine, index) => {
+        if(comparedCodeInfoMap.modifiedList.indexOf(index) >= 0) {
+            currentCodeContainer.append('<p class="modified_code">' + currentLine + '</p>');
+        } else if(comparedCodeInfoMap.addedList.indexOf(index) >= 0) {
+            currentCodeContainer.append('<p class="added_code">' + currentLine + '</p>');
+        } else {
+            currentCodeContainer.append('<p>' + currentLine + '</p>');
+        }
+    });
 }
 
+const goToInsertCode = function() {
+    const projectId = $('#currentProjectId').val();
+    const form = $('#InsertCodeForm');
+
+    $('#projectIdForCode').val(projectId);
+
+    form.submit();
+}
