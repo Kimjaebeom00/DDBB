6f2c8a24c2bcd6acb7f0453589f7fbdd72f3c1c8
Jaehyeok Eom <djawogur2018@naver.com>
2023-05-18 17:38:36 +0900

    코드 비교 기능 고도화 테스트 코드 로직 작성했습니다. MAP 가공 전 테스트 완료 했습니다.

---
+++ src/main/java/com/project/ddbb/domain/vo/DiffData.java
@@ -0,0 +1,14 @@
+package com.project.ddbb.domain.vo;
+
+public class DiffData
+{
+    public int Length;
+    public int[] data;
+    public boolean[] modified;
+
+    public DiffData(int[] initData) {
+        data = initData;
+        Length = initData.length;
+        modified = new boolean[Length + 2];
+    }
+}
